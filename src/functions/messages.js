ec145e7c3da01a2df275c132df047c290c1fac7c
codealoalex <alexAvalosMS@outlook.com>
2026-06-12 08:38:49 -0400

    Subiendo prueba

--- src/functions/messages.js
+++ src/functions/messages.js
@@ -5,9 +5,7 @@ app.http('messages', {
     authLevel: 'anonymous',
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
-
-        const name = request.query.get('name') || await request.text() || 'world';
-
+        const name = "alex";
         return { body: `Hello, ${name}!` };
     }
 });
